7248b11ecd64594ce48ce3e1d895d583b4224fdc
Sebastian Salgado <sebasalgado402@gmail.com>
2023-06-26 22:06:17 -0300

    se arreglo busqueda en gastos

--- ecommerce/articulo.php
+++ ecommerce/articulo.php
@@ -14,11 +14,11 @@
     <!--Icono en la pestaña -->
     <link rel="shortcut icon" href="./../assets/icons/favicon.png">
     <!--Iconos de bootstrap  -->
-    <link rel="stylesheet" href="./../bootstrapIcons/font/bootstrap-icons.css">
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"> -->
+    <!-- <link rel="stylesheet" href="./../bootstrapIcons/font/bootstrap-icons.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <!--Estilos Bootstrap -->
-    <link href="./../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" > -->
+    <!-- <link href="./../bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" >
     <!-- Estilos jQueryUI -->
     <link rel="stylesheet" href="./../jQueryUI/jquery-ui.css">
     <link rel="stylesheet" href="./../jQueryUI/jquery-ui.structure.css">
@@ -44,13 +44,13 @@
     <?php include('./../assets/Components/footer.php'); ?>
 </body>
 <!--Importa librería jquery -->
-<script src="./../jQuery/jquery.min.js"></script>
-<!-- <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script> -->
+<!-- <script src="./../jQuery/jquery.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
 <!--Importa librería jQueryUI -->
-<script src="./../jQueryUI/jquery-ui.min.js"></script>
+<!-- <script src="./../jQueryUI/jquery-ui.min.js"></script> -->
 <!--Importa librería boostrap -->
 <script src="./../bootstrap/js/bootstrap.min.js"></script>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 <!--Importo javascript propio -->
 <script src="./../assets/js/functions.js"></script>
 <!--  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
